a33f01482245a9d89f2855192d61b2cd36065ce0
jaiminchandarana <137302744+jaiminchandarana@users.noreply.github.com>
2025-01-31 13:59:49 +0530

    Delete backup.py

--- backup.py
+++
@@ -1,7 +0,0 @@
-with open(f'C:\\Users\\Jaimin\\OneDrive\\Desktop\\jarvis\\Jarvis_copy.py','r') as mainfile:
-    content = mainfile.read()
-    
-with open(f'C:\\Users\\Jaimin\\OneDrive\\Desktop\\jarvis\\JarvisCopy.py','w') as contentfile:
-    contentfile.write(content)
-    
-print("Content copied!!")
