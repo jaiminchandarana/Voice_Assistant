f630c46816b9a19cfefa81755db1b87d6cf9f068
jaiminchandarana <137302744+jaiminchandarana@users.noreply.github.com>
2025-04-18 15:12:13 +0530

    Update scrapewebpage.py

--- scrapewebpage.py
+++ scrapewebpage.py
@@ -4,7 +4,7 @@ import re
 import google.generativeai as genai
 
 def scrape():
-    genai.configure(api_key="AIzaSyCUv38iXDaLaVKUopLtFHkp9OkF3Zgabgw")
+    genai.configure(api_key="")
     def main():
         speak("Provide me your URL sir.")
         url = input("Enter your URL : ")
